8d8a9a1a0b23b80880b350901d742dd60ba0481c
Shreyagoel27 <shreyagoel098@gmail.com>
2022-03-26 16:42:08 +0530

    Common work done

---
+++ client/src/components/Banner/Banner.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <div className="banner">
+      <div className="bannersec">
+        <div className="headingban">
+          <div>
+            <h1>7Fit</h1>
+            <p>Fitness hack for HackFit</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
